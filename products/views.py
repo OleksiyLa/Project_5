238682a16554e9f6e665039c0146d109e15c4aef
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-12-22 19:15:24 +0000

    feature: add pizza search functionality on the pizza list page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Product
 from .forms import AddProduct, PizzaFilterForm
 
@@ -6,30 +7,34 @@ from .forms import AddProduct, PizzaFilterForm
 
 
 def pizza_list(request):
-    form = PizzaFilterForm(request.GET)
-    if form.is_valid():
-        is_spicy = form.cleaned_data.get('is_spicy')
-        is_vegetarian = form.cleaned_data.get('is_vegetarian')
-        is_premium = form.cleaned_data.get('is_premium')
-        is_seafood = form.cleaned_data.get('is_seafood')
-        is_new = form.cleaned_data.get('is_new')
+    products = Product.objects.all()
+    query = request.GET.get('q')
+    if query:
+        form = PizzaFilterForm()
+        products = products.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    else:
+        form = PizzaFilterForm(request.GET)
+        if form.is_valid():
+            is_spicy = form.cleaned_data.get('is_spicy')
+            is_vegetarian = form.cleaned_data.get('is_vegetarian')
+            is_premium = form.cleaned_data.get('is_premium')
+            is_seafood = form.cleaned_data.get('is_seafood')
+            is_new = form.cleaned_data.get('is_new')
 
-        filters = {}
-        if is_spicy:
-            filters['is_spicy'] = is_spicy
-        if is_vegetarian:
-            filters['is_vegetarian'] = is_vegetarian
-        if is_premium:
-            filters['is_premium'] = is_premium
-        if is_seafood:
-            filters['is_seafood'] = is_seafood
-        if is_new:
-            filters['is_new'] = is_new
+            filters = {}
+            if is_spicy:
+                filters['is_spicy'] = is_spicy
+            if is_vegetarian:
+                filters['is_vegetarian'] = is_vegetarian
+            if is_premium:
+                filters['is_premium'] = is_premium
+            if is_seafood:
+                filters['is_seafood'] = is_seafood
+            if is_new:
+                filters['is_new'] = is_new
 
-        if any(filters.values()):
-            products = Product.objects.filter(**filters)
-        else:
-            products = Product.objects.all()
+            if any(filters.values()):
+                products = products.filter(**filters)
 
     return render(request, 'products/pizza_list.html', {'products': products, 'form': form})
 
