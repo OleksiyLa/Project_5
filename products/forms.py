e2575be9b30880fcff6c30229275ab9b1b30dc75
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-12-22 18:20:05 +0000

    feature: add filter for pizza

--- products/forms.py
+++ products/forms.py
@@ -7,3 +7,10 @@ class AddProduct(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'price', 'rating', 'image', 'is_spicy', 'is_vegetarian', 'is_premium', 'is_seafood', 'is_new']
+
+class PizzaFilterForm(forms.Form):
+    is_spicy = forms.BooleanField(label='Spicy', required=False)
+    is_vegetarian = forms.BooleanField(label='Vegetarian', required=False)
+    is_premium = forms.BooleanField(label='Premium', required=False)
+    is_seafood = forms.BooleanField(label='Seafood', required=False)
+    is_new = forms.BooleanField(label='New', required=False)
--- products/views.py
+++ products/views.py
@@ -1,13 +1,38 @@
 from django.shortcuts import render, redirect
 from .models import Product
-from .forms import AddProduct
+from .forms import AddProduct, PizzaFilterForm
 
 # Create your views here.
 
 
 def pizza_list(request):
-    products = Product.objects.all()
-    return render(request, 'products/pizza_list.html', {'products': products})
+    form = PizzaFilterForm(request.GET)
+    if form.is_valid():
+        is_spicy = form.cleaned_data.get('is_spicy')
+        is_vegetarian = form.cleaned_data.get('is_vegetarian')
+        is_premium = form.cleaned_data.get('is_premium')
+        is_seafood = form.cleaned_data.get('is_seafood')
+        is_new = form.cleaned_data.get('is_new')
+
+        filters = {}
+        if is_spicy:
+            filters['is_spicy'] = is_spicy
+        if is_vegetarian:
+            filters['is_vegetarian'] = is_vegetarian
+        if is_premium:
+            filters['is_premium'] = is_premium
+        if is_seafood:
+            filters['is_seafood'] = is_seafood
+        if is_new:
+            filters['is_new'] = is_new
+
+        if any(filters.values()):
+            products = Product.objects.filter(**filters)
+        else:
+            products = Product.objects.all()
+
+    return render(request, 'products/pizza_list.html', {'products': products, 'form': form})
+
 
 def add_pizza(request):
     form = AddProduct()
