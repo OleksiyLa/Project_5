41018d7aefc43fe4907e2b82923c807a3a1fae8e
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-12-30 16:50:10 +0000

    feature: add stripe_elements.js and checkout.css

--- super_pizza/settings.py
+++ super_pizza/settings.py
@@ -173,3 +173,6 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
