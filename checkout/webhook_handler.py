5bfc6cde3890e4d08af3d2ecfe40b73afedd8bac
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-01 21:36:45 +0000

    update webhook_handler, add addititonal fields to admin (checkout)

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -6,6 +6,7 @@ from decimal import Decimal
 
 import json
 import time
+import stripe
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -30,9 +31,13 @@ class StripeWH_Handler:
         bag = intent.metadata.bag
         save_info = intent.metadata.save_info
 
-        billing_details = intent.charges.data[0].billing_details
+        stripe_charge = stripe.Charge.retrieve(
+            intent.latest_charge
+        )
+
+        billing_details = stripe_charge.billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.charges.data[0].amount / 100, 2)
+        grand_total = round(stripe_charge.amount / 100, 2)
 
         # Clean data in the shipping details
         for field, value in shipping_details.address.items():
