328482205b559326bd869bd213fef9a232e18cc9
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-02 21:54:16 +0000

    feature: create users.context_processors to check wether user is Worker and display Orders nav link if it is

--- super_pizza/settings.py
+++ super_pizza/settings.py
@@ -88,6 +88,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
+                'users.context_processors.worker_status',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
