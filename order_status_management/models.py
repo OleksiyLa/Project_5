1d193b57188116aa00e7ab8b4fd2069638a402a4
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-03 13:59:09 +0000

    feature: add time to relevant field when status changed in the model, migrate

--- order_status_management/models.py
+++ order_status_management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class OrderProgress(models.Model):
@@ -12,12 +13,29 @@ class OrderProgress(models.Model):
     ]
 
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='new')
-    new_at = models.DateTimeField(auto_now_add=True)
+    new_at = models.DateTimeField(null=True, blank=True)
     accepted_at = models.DateTimeField(null=True, blank=True)
     start_cooking_at = models.DateTimeField(null=True, blank=True)
     start_preparing_at = models.DateTimeField(null=True, blank=True)
     start_delivering_at = models.DateTimeField(null=True, blank=True)
     completed_at = models.DateTimeField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            if self.status == 'new':
+                self.new_at = timezone.now()
+            elif self.status == 'accepted':
+                self.accepted_at = timezone.now()
+            elif self.status == 'being_cooked':
+                self.start_cooking_at = timezone.now()
+            elif self.status == 'being_prepared':
+                self.start_preparing_at = timezone.now()
+            elif self.status == 'being_delivered':
+                self.start_delivering_at = timezone.now()
+            elif self.status == 'completed':
+                self.completed_at = timezone.now()
+        
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"OrderProgress - {self.get_status_display()}"
