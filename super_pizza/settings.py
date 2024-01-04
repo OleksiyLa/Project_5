93ef287b8403661c06e11bf76f408fd9e2ef9d95
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-04 13:44:57 +0000

    feature: test user profiel, add link to profile page

--- super_pizza/settings.py
+++ super_pizza/settings.py
@@ -136,13 +136,6 @@ WSGI_APPLICATION = 'super_pizza.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': dj_database_url.parse(config('DATABASE_URL'))
 }
