f7c7e910ed48502da0fd4d200c31ad97d647cfd0
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-05 18:22:05 +0000

    feature: add 404 page

--- super_pizza/urls.py
+++ super_pizza/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +29,5 @@ urlpatterns = [
     path('order_tracker/', include('order_tracker.urls')),
     path('profiles/', include('profiles.urls')),
 ]
+
+handler404 = 'boutique_ado.views.handler404'
