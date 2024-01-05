235f3c084babe4f0c3cfafdc355863847d194bb7
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-05 17:41:57 +0000

    feature: use config instead of os.environ in settings

--- super_pizza/settings.py
+++ super_pizza/settings.py
@@ -190,7 +190,8 @@ STRIPE_PUBLIC_KEY = config('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = config('STRIPE_SECRET_KEY')
 STRIPE_WH_SECRET = config('STRIPE_WH_SECRET')
 
-if 'DEVELOPMENT' in os.environ:
+DEVELOPMENT = config('DEVELOPMENT', default=False, cast=bool)
+if DEVELOPMENT:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'superpizza@example.com'
 else:
@@ -198,6 +199,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = config('EMAIL_HOST_USER')
