3cb11ed45f963c2538abc4a92862c8a3cef07540
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-05 21:48:06 +0000

    feature: handle order tracking with invalid order number

--- order_tracker/views.py
+++ order_tracker/views.py
@@ -1,16 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from checkout.models import Order
+
 
 def order_tracker(request):
     if request.POST:
         order_number = request.POST['order_number']
-        return redirect('order_tracker_bar', order_number) 
+        order = Order.objects.filter(order_number=order_number).exists()
+
+        if order:
+            order = Order.objects.get(order_number=order_number)
+            if order.progress.is_active:
+                return redirect('order_tracker_bar', order_number=order.order_number)
+            else:
+                messages.error(request, 'Order not found')
+                return redirect('order_tracker')
+        else:
+            messages.error(request, 'Order not found')
+            return redirect('order_tracker')
 
     return render(request, 'order_tracker/order_tracker.html', {'active_link': 'tracker'})
 
 
 def order_tracker_bar(request, order_number):    
-    order = Order.objects.get(order_number=order_number)
+    order = get_object_or_404(Order, order_number=order_number, progress__is_active=True)
     status = order.progress.status
 
     time_taken = None
