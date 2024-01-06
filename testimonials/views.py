9c16bcff28025e4efc226dd5881053d3631cc571
Oleksiy Lazarenko <alexusunit@gmail.com>
2024-01-06 16:52:03 +0000

    featrue: complete provide testimonials functionality when user has at least one order

--- testimonials/views.py
+++ testimonials/views.py
@@ -9,20 +9,22 @@ def testimonials_view(request):
     return render(request, 'testimonials/testimonials.html', {'active_link': 'testimonials'})
 
 
+@login_required
 def add_testimonial(request):
     if request.method == 'POST':
-        form = TestimonialForm(request.POST)
-        if form.is_valid():
-            testimonial = form.save(commit=False)
-            testimonial.user = request.user
-            testimonial.save()
-            print("valid", form.cleaned_data)
-            messages.success(request, 'Thank you for your testimonial!')
-            return redirect('testimonials')
+        user = request.user
+        orders = user.userprofile.orders.all()
+        if orders.count() > 0:
+            form = TestimonialForm(request.POST)
+            if form.is_valid():
+                testimonial = form.save(commit=False)
+                testimonial.user = request.user
+                testimonial.save()
+                messages.success(request, 'Thank you for your testimonial!')
+                return redirect('testimonials')
         else:
-            messages.error(request, 'Form is invalid.')
-            print("error", form.cleaned_data)
+            messages.error(request, 'You must have placed an order to leave a testimonial.')
+            return redirect('testimonials')
     else:
         form = TestimonialForm()
     return render(request, 'testimonials/add_testimonial.html', {'form': form})
-
